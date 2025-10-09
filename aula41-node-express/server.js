57c9b513dcad9a7de4ea92f041424b76e0ac8c92
Osvaldo Souza <osvaldo.ark@gmail.com>
2025-10-09 05:33:32 -0300

    feat: adicionando a aula de node crud 01

--- aula41-node-express/server.js
+++ aula41-node-express/server.js
@@ -1,23 +1,74 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const port = 3000;
 
 const app = express();
 
+let id = 1;
+const clientes = [	{
+		"nome": "Agatha",
+		"idade": 31,
+		"endereco": "papicu",
+		"id": 1
+	}];
+
+app.use(json());
+
 app.get('/',(requisicao,resposta)=>{
-    resposta.json({"messagem":"criando uma rota com express"});
+    resposta.json(clientes);
 });
 
-app.get('/numero-da-sorte/:numero/:numeroazar',(requisicao,resposta)=>{
+app.get('/cliente/:id',(requisicao,resposta)=>{
 
-    const parametro = requisicao.params.numero;
+    const {id} = requisicao.params;
 
-    const outroParametro = requisicao.params.numeroazar;
+    const [resultado] = clientes.filter(cliente => cliente.id === Number.parseInt(id));
 
-    const {numero,numeroazar} = requisicao.params;
-
-    resposta.send(`esse é o número da sorte: ${parametro} - ${outroParametro}`);
+    if(resultado !== undefined){
+        resposta.status(200).json(resultado);
+    }
+    else{
+        resposta.status(404).json({"erro":"cliente não encontrado"});
+    }
 });
+
+app.post('/',(requisicao,resposta)=>{
+
+    //const cliente = requisicao.body.cliente;
+    let { cliente } = requisicao.body;
+
+    cliente = {...cliente,"id":id}
+
+    id++;
+
+    clientes.push(cliente);
+
+    resposta.status(200).json(cliente);
+
+});
+
+app.put('/:id',(requisicao,resposta)=>{
+
+    const {id} = requisicao.params;
+
+    const { cliente } = requisicao.body
+
+    let [resultado] = clientes.filter(cliente => cliente.id === Number.parseInt(id));
+
+    if(resultado !== undefined){
+        for(let propriedade in resultado){
+
+            if(cliente[propriedade]!== undefined)
+                resultado[propriedade] = cliente[propriedade];
+        }
+
+        resposta.status(200).json(resultado);
+    }    
+    else{
+        resposta.status(404).json({"erro":"cliente não encontrado"});
+    }
+});
+
 
 app.listen(port,()=>{
     console.log(`o   servidor está rodando em http://127.0.0.1:${port}`);
